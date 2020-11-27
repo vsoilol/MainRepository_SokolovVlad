06987ee78d3788692bd344829b65df26245cc89d
Vlad <vsoilol1701@gmail.com>
2020-11-27 18:54:21 +0300

    Добавьте файлы проекта.

---
+++ EPAMHomework_SokolovVlad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAMHomework_SokolovVlad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
